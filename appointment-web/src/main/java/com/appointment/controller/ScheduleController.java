623a20e2b660adb1b1d3457ead79a643f5cb21b3
Parag Shrivastava <parag.shrivastav@gmail.com>
2014-06-26 10:09:07 +0200

    Adding missed file in last checkin due to missing git add step

---
+++ appointment-web/src/main/java/com/appointment/controller/ScheduleController.java
@@ -0,0 +1,16 @@
+package com.appointment.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ScheduleController {
+	
+	/*@RequestMapping(value = "/{name}", method = RequestMethod.GET)
+	public String getMovie(@PathVariable String name, ModelMap model) {
+ 
+		model.addAttribute("movie", name);
+		return "list";
+ 
+	}*/
+
+}
