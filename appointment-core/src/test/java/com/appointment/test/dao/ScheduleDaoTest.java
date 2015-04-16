348025910c84ee2f3c60f003da68248693bdca62
amitthapar123 <amitthapar123@gmail.com>
2015-04-16 18:25:43 +0530

    Added after running test cases for DAO and Service

--- appointment-core/src/test/java/com/appointment/test/dao/ScheduleDaoTest.java
+++ appointment-core/src/test/java/com/appointment/test/dao/ScheduleDaoTest.java
@@ -1,12 +1,17 @@
 package com.appointment.test.dao;
 
+import java.util.List;
+
 import org.apache.log4j.BasicConfigurator;
 import org.apache.log4j.Logger;
 import org.bson.types.ObjectId;
+import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.appointment.dao.BaseDao;
+import com.appointment.domain.Config;
 import com.appointment.domain.Schedule;
 
 public class ScheduleDaoTest {
@@ -23,13 +28,110 @@ public class ScheduleDaoTest {
 	public void setUp() throws Exception {
 		// @Todo Run mongo with a test specific .js file to produce initial data
 		// state
-		logger.info("setting up test");
 		BasicConfigurator.configure();
 
 		dao = (BaseDao<Schedule>) MyTestApplicationContext.getInstance()
 				.getBean("scheduleDao");
 	}
 
+	
+	@Test
+	public void testSchedule_Insert() {
+		Schedule Schedule = createSchedule();
+
+		Schedule insertedSchedule = dao.insert(Schedule);
+
+		Schedule insertedSch = dao.selectByPk(insertedSchedule.getId(),
+				Schedule.class);
+		Assert.assertNotNull(" Schedule inserted", insertedSch);
+	}
+
+	@Test
+	public void testSchedule_Delete() {
+		Schedule Schedule = createSchedule();
+
+		Schedule insertedSchedule = dao.insert(Schedule);
+
+		Schedule insertedSch = dao
+				.selectByPk(Schedule.getId(), Schedule.class);
+		dao.delete(insertedSch, Schedule.class);
+		Schedule deleteSch = dao.selectByPk(insertedSchedule.getId(),
+				Schedule.class);
+		Assert.assertNull("Schedule deleted", deleteSch);
+	}
+
+	@Test
+	public void testSchedule_Find() {
+		Schedule Schedule = createSchedule();
+
+		Schedule insertedSchedule = dao.insert(Schedule);
+
+		Schedule fetchSch = dao.selectByPk(insertedSchedule.getId(),
+				Schedule.class);
+		Assert.assertTrue("fetched and created should be same", fetchSch
+				.getId().equals(Schedule.getId()));
+	}
+
+	@Test
+	public void testSchedule_FindAll() {
+		Schedule Schedule = createSchedule();
+
+		List<Schedule> fetched = dao.findAll(Schedule.class);
+
+		Assert.assertTrue("fetched size should be greater than zero",
+				fetched.size() > 0);
+	}
+
+	@Test
+	public void testSchedule_CountAll() {
+		Schedule Schedule = createSchedule();
+
+		long recordCount = dao.countAll(Schedule.class);
+
+		Assert.assertTrue("fetched size should be greater than zero",
+				recordCount > 0);
+	}
+	
+
+	@Test
+	public void testSchedule_Update() {
+		Schedule Schedule = createSchedule();
+
+		Schedule insertedSchedule = dao.insert(Schedule);
+		insertedSchedule.setDurationId(new Long(2));
+		Schedule fetchSch = dao.update(insertedSchedule);
+		Assert.assertTrue("fetched and created should be same", fetchSch
+				.getId().equals(insertedSchedule.getId()));
+	}
+	
+	
+
+	private Schedule createSchedule() {
+		Schedule schedule = new Schedule();
+		schedule.setConfig(this.createConfig());
+		schedule.setDuration("1");
+		schedule.setDurationId(new Long(1));
+		schedule.setId(new ObjectId());
+		schedule.setResourceCount("count");
+		schedule.setStatus("status");
+		schedule.setThreshold("Threshold");
+			
+		return schedule;
+	}
+	
+	private Config createConfig() {
+		Config config = new Config();
+		config.setEndHr("8");
+		config.setFrequency("frequency");
+		config.setMinSlotPerResource("1");
+		config.setResources("Test");
+		config.setStartHr("1");
+		
+		return config;
+	}
+	
+
+	
 	// @BeforeClass
 	public static void beforeClass() throws Exception {
 
