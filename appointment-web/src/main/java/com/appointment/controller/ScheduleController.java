90da6570a6f3c8bcb6e5056aa8a6ac9ccab8a534
SinghDP <D.P.Singh@rn.rabobank.nl>
2015-04-20 23:15:58 +0200

    Schedule Rest Service Expose

--- appointment-web/src/main/java/com/appointment/controller/ScheduleController.java
+++ appointment-web/src/main/java/com/appointment/controller/ScheduleController.java
@@ -1,16 +1,35 @@
 package com.appointment.controller;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.appointment.domain.Schedule;
+import com.appointment.services.BaseService;
+
+@RestController
 public class ScheduleController {
-	
-	/*@RequestMapping(value = "/{name}", method = RequestMethod.GET)
-	public String getMovie(@PathVariable String name, ModelMap model) {
- 
-		model.addAttribute("movie", name);
-		return "list";
- 
-	}*/
+
+	/*
+	 * @RequestMapping(value = "/{name}", method = RequestMethod.GET) public
+	 * String getMovie(@PathVariable String name, ModelMap model) {
+	 * 
+	 * model.addAttribute("movie", name); return "list";
+	 * 
+	 * }
+	 */
+	@Autowired
+	private BaseService<Schedule> service;
+
+	@RequestMapping(value = URIConstants.GET_ALL_SCHEDULE, method = RequestMethod.GET)
+	public List<Schedule> getAllSchedules() {
+
+		List<Schedule> schedules = service.getAll(Schedule.class);
+
+		return schedules;
+	}
 
 }
