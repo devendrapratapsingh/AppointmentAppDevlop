5c4ea97d1037a0115ff6b6104f7cc62d357835ba
SinghDP <D.P.Singh@rn.rabobank.nl>
2015-04-20 23:48:02 +0200

    changes to connect correct DB using spring

--- appointment-web/src/main/java/com/appointment/config/AppConfig.java
+++ appointment-web/src/main/java/com/appointment/config/AppConfig.java
@@ -1,17 +1,30 @@
 package com.appointment.config;
 
-import java.net.UnknownHostException;
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDbFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoDbFactory;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
-import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.appointment")
 public class AppConfig {
-	
+	public @Bean
+	MongoDbFactory mongoDbFactory() throws Exception {
+		return new SimpleMongoDbFactory(new MongoClient(), "appointment");
+	}
+ 
+	public @Bean
+	MongoTemplate mongoTemplate() throws Exception {
+ 
+		MongoTemplate mongoTemplate = new MongoTemplate(mongoDbFactory());
+ 
+		return mongoTemplate;
+ 
+	}
 }
